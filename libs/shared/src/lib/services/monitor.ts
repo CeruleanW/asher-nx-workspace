a1569fda80588b9d5e1c9dca54d65347dc849b48
Asher <ayang@safetrack.ai>
2023-09-18 22:50:01 -0400

    upgrade sentry

--- libs/shared/src/lib/services/monitor.ts
+++ libs/shared/src/lib/services/monitor.ts
@@ -2,12 +2,11 @@
  * Measure & collect issues from clients
  */
 import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
 
 export function initMonitoring() {
   Sentry.init({
     dsn: "",
-    integrations: [new BrowserTracing()],
+    integrations: [new Sentry.BrowserTracing(),],
 
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
