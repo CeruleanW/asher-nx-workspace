a52f7919b14db756d7e55b31483f93df6a0bb790
Asher <ayang@safetrack.ai>
2024-02-07 22:19:00 -0500

    fix yarn dep errors again

--- apps/google-photos-search-enhancer/src/App.tsx
+++ apps/google-photos-search-enhancer/src/App.tsx
@@ -1,7 +1,11 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
 import {Footer} from './components/atomic/Footer';
 import { Main } from './components/Main';
-import { CompositeProvider } from './providers'
+import { CompositeProvider } from './providers';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 /**
  *
