0554a535c6c998b1e96dbc3014f23b7e2f03d82f
CeruleanW <ashery.tech@gmail.com>
2026-02-15 14:53:25 -0500

    remove API key in code

--- apps/portfolio/src/features/firebase/config.ts
+++ apps/portfolio/src/features/firebase/config.ts
@@ -6,7 +6,7 @@ import { getStorage, ref } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAeqQEL_IdLRwOejvvJYRVO9kh5dgsfpgU",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "asher-personal-data.firebaseapp.com",
   projectId: "asher-personal-data",
   storageBucket: "asher-personal-data.appspot.com",
